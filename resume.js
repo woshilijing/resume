02979c490ad3a44304becb2b54bc07704e95d277
lijing <949744503@qq.com>
2016-03-28 14:40:00 +0800

    我的简历

--- resume.js
+++ resume.js
@@ -1,6 +1,61 @@
 /**
  * Created by LJ on 2016/3/21.
  */
+
+function getEle(ele){
+    return document.querySelector(ele);
+}
+var num = 0;
+var progress=document.querySelector(".progress");
+var loading=document.querySelector("#loading");
+function fnLoad() {
+    var arr=['sunny.png','cloud1.png','city1.png','right.png','left.png',
+        'map2.png','me1.png','skills2.png','position.png','js.png','ajax.png',
+    'c1.png','c6.png','nodejs.png','c2.png'];
+    if (!arr) {
+        return;
+    }
+    for(var i= 0,len=arr.length;i<len;i++){
+        var oImg=new Image;
+        oImg.src="images/"+arr[i];
+        oImg.onload=function(){
+            num++;
+            progress.style.width=(num/arr.length)*100+"%";
+            if(num==arr.length&&loading){
+                main.removeChild(loading);
+                loading=null;
+                var oDiv=document.querySelector(".a0");
+                oDiv.firstElementChild.id="p0";
+                oDiv.lastElementChild.id="p1";
+            }
+        }
+
+    }
+}
+fnLoad();
+
+var audio=getEle(".audio");
+var music=getEle(".music");
+window.addEventListener("load",function(){
+    music.play();
+    music.addEventListener("canplay",function(){
+        audio.style.display="block";
+        audio.id="move";
+    },false);
+    audio.addEventListener("touchend",function(){
+        if(music.paused){
+            music.play();
+            audio.id="move";
+        }else{
+            music.pause();
+            audio.id="";
+        }
+
+    })
+
+},false);
+
+
 var main=document.querySelector("#main");
 var oLis=document.querySelectorAll("#list>li");
 
@@ -13,6 +68,9 @@ if(winW/winH<=desW/desH){
 }else{
     main.style.webkitTransform="scale("+winW/desW+")";
 }
+document.body.addEventListener("touchstart",function(e){
+    e.preventDefault();
+},false);
 [].forEach.call(oLis,function(){
     var oLi=arguments[0];
     oLi.index=arguments[1];
@@ -41,23 +99,27 @@ function move(e){
         arguments[0].className="";//每一张的zIndex都要清掉
     });
 
-    if(pos>=0){
+    if(pos>0){
         this.prevsIndex=(index===0?oLis.length-1:index-1);
         var duration=-winH+pos;
+        oLis[index].style.webkitTransformOrigin = "bottom";
+        //由于每一张都脱离了文档流，所以在这个要加
 
     }else if(pos){
         this.prevsIndex=(index===oLis.length-1?0:index+1);
-        var duration=winH+pos;
+        duration=winH+pos;
+        oLis[index].style.webkitTransformOrigin = "top";
     }
-    oLis[this.prevsIndex].style.display="block";
+
     oLis[this.prevsIndex].style.webkitTransform="translate(0,"+duration+"px)";
     oLis[this.prevsIndex].className="zIndex";
+    oLis[this.prevsIndex].style.display="block";
     //oLis[index].style.webkitTransform="scale("+(1-(Math.abs(pos)/winH)*1/2)+") translate(0,"+pos+"px)"
-
+    oLis[index].style.webkitTransform = "scale(" + (1 - Math.abs(pos / winH) * 0.4) + ") translate(0," + pos / 4 + "px)";
 }
 function end(e){
     oLis[this.prevsIndex].style.webkitTransform="translate(0,0)";
-    oLis[this.prevsIndex].style.webkitTransition="1s";
+    oLis[this.prevsIndex].style.webkitTransition="0.5s";
     oLis[this.prevsIndex].addEventListener("webkitTransitionEnd",function(){
         this.style.webkitTransition="";
         if(this==oLis[0]){
